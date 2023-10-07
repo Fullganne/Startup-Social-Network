339c11600c71de4b12d398a6cc6f21e225986061
DESKTOP-A02GDQH\DELL <phuclethinh70@gmail.com>
2023-10-07 22:28:41 +0700

    f

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -3,15 +3,13 @@ import {useState} from 'react';
 const Login=()=>{
     const [name,setName]=useState("");
     const [gmail,setGmail] = useState("");
-    
+
     const change=(e)=>{
         setName(e.target.value);
         console.log(name);
     }
     return(
         <div>
-            <input className='abc' onChange={change}></input>
-            <input></input>
 
         </div>
     );
