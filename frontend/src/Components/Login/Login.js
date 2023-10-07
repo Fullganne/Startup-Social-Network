05c563a4659060c158370e64e56c9b7162e67cee
DESKTOP-A02GDQH\DELL <phuclethinh70@gmail.com>
2023-10-07 22:30:53 +0700

    f

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,18 +1,6 @@
 import './Login.css';
 import {useState} from 'react';
 const Login=()=>{
-    const [name,setName]=useState("");
-    const [gmail,setGmail] = useState("");
-
-    const change=(e)=>{
-        setName(e.target.value);
-        console.log(name);
-    }
-    return(
-        <div>
-
-        </div>
-    );
 }
 
 export default Login;
