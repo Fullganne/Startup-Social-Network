d29f9087f912fb350de2e2abf00717978cabb35c
DuyTan <zkp.hades@gmail.com>
2023-11-06 19:18:56 +0700

    Make postCard

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import PostCard from '../../Components/Post/post'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <div className='mt-10 flex w-[100%] justify-center'>
+        <div className='w-[44%] px-10'>
+            <div className='space-y-10 w-full mt-10'>
+              <PostCard></PostCard>
+            </div>
+        </div>
+        <div className='w-[35%]'>
+            homeright
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default HomePage
+// const Profile = () => {
+//   return (
+//     <div className='px-20'>
+//       <div >
+//         <ProfileUserDetails></ProfileUserDetails>
+//       </div>
+//       <div>
+//         <ReqUserPostPart/>
+//       </div>
+//     </div>
+//   )
+// }
