0adc868d4bc53f137f18010f732be94a72536401
Fullganne <firewolf148@gmail.com>
2023-11-22 16:14:50 +0700

    make-validate-sign-in-and-sign-up-page

--- frontend/src/Components/signUp/signUp.js
+++ frontend/src/Components/signUp/signUp.js
@@ -15,33 +15,99 @@ const SignUp=()=>{
     const [email,setEmail]=useState("");
     const [username,setUsername]=useState("");
     const [password,setPassword]=useState("");
-
+    const [errors,setErrors]=useState("");
     const navigate=useNavigate();
    
-    const handleSignup= async ()=>{
-        try{
-            // if()
-            if(username&&password&&email){
-                const newUser=await userService.signupUser({id:uuid4(),username:username,email:email,password:password})
+    // const handleSignup= async ()=>{
+    //     try{
+    //         // if()
+    //         if(username&&password&&email){
+    //             const newUser=await userService.signupUser({id:uuid4(),username:username,email:email,password:password})
 
-                setEmail(''); setPassword(''); setUsername('');
-                console.log(newUser);
+    //             setEmail(''); setPassword(''); setUsername('');
+    //             console.log(newUser);
 
-                if(newUser?.data?.message){
+    //             if(newUser?.data?.message){
 
-                }else{
-                    alert("Tao tai khoan thanh cong")
-                    navigate('/')
-                }
-            }else{
-                alert("Ban chua dien day du thong tin")
+    //             }else{
+    //                 alert("Tao tai khoan thanh cong")
+    //                 navigate('/')
+    //             }
+    //         }else{
+    //             alert("Ban chua dien day du thong tin")
+    //         }
+    //     }catch(e){
+    //         console.log(e.response.data);
+    //         alert(e.response.data)
+    //     }
+    // }
+
+    const validatePassword = (password) => {
+        const hasUpperCase = /[A-Z]/.test(password); 
+        const hasLowerCase = /[a-z]/.test(password); 
+        const hasNumber = /\d/.test(password);
+        const hasSpecialChar = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password);
+
+        const isValidLength = password.length >= 6;
+      
+        return hasUpperCase && hasLowerCase && hasNumber && isValidLength && hasSpecialChar;
+      };
+
+    const handleSignup = async () => {
+        try {
+            const validationErrors = {}; 
+
+            if (!username.trim()) {
+                validationErrors.username = 'Username không được để trống!';
             }
-        }catch(e){
-            console.log(e.response.data);
-            alert(e.response.data)
+            if (!email.trim()) {
+                validationErrors.email = 'Email không được để trống!';
+            } else if (!/\S+@\S+\.\S+/.test(email)) {
+                validationErrors.email = 'Email không hợp lệ!';
+            }
+            if (!password.trim()) {
+                validationErrors.password = 'Password không được để trống!';
+            } else if(!validatePassword(password)) {
+                validationErrors.password = 'Password cần ít nhất 6 kí tự, chữ hoa, thường và kí tự đặc biệt!';
+            }
+        
+            if (Object.keys(validationErrors).length === 0) {
+                const emailExists = await userService.checkEmailExists(email);
+                if(emailExists)
+                {
+                    validationErrors.email = 'Email này đã tồn tại!';
+                    setErrors(validationErrors);
+                }
+                else {
+                    const newUser = await userService.signupUser({
+                        id: uuid4(),
+                        username: username,
+                        email: email,
+                        password: password,
+                    });
+            
+                    setEmail('');
+                    setPassword('');
+                    setUsername('');
+                    console.log(newUser);
+            
+                    if (newUser?.data?.message) {
+                    // Handle message if needed
+                    } else {
+                        alert('Tạo tài khoản thành công');
+                        navigate('/');
+                    }
+                }
+            }
+            else {
+                setErrors(validationErrors);
+          }
         }
-    }
-
+        catch (e) {
+            console.log(e.response.data);
+            alert(e.response.data);
+        }
+      };
 
     return(
     <div className='body'>
@@ -59,19 +125,24 @@ const SignUp=()=>{
             <div className='inputs'>
                 <div className='input'>
                     <img src={email_icon} alt=''/>
-                    <input type='text' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Nhập Email'/>
+                    <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Nhập Email'/>
                 </div>
-                {/* <div className='input'>
-                    <img src={user_icon} alt=''/>
-                    <input type='fullname' placeholder='Nhập tên đầy đủ'/>
-                </div> */}
+                <div className='flex w-[80%] m-auto px-5'>
+                {errors.email && <span className="error text-red-500">{errors.email}</span>}
+                </div>
                 <div className='input'>
                     <img src={user_icon} alt=''/>
                     <input value={username} onChange={(e)=>setUsername(e.target.value)} type='text' placeholder='Nhập Username'/>
                 </div>
+                <div className='flex w-[80%] m-auto px-5'>
+                    {errors.username && <span className="error text-red-500">{errors.username}</span>}
+                </div>
                 <div className='input'>
                     <img src={password_icon} alt=''/>
                     <input value={password} onChange={(e)=>setPassword(e.target.value)} type='password' placeholder='Nhập Password'/>
+                </div>
+                <div className='flex w-[80%] m-auto px-5'>
+                    {errors.password && <span className="error text-red-500 text-left">{errors.password}</span>}
                 </div>
             </div>
             <div style={{textAlign:"center", width:"80%",margin:"auto",paddingTop:"30px"}}> <span>Những người dùng dịch vụ của chúng tôi có thể đã tải thông tin liên hệ của bạn lên Instagram. <a href='#'>Tìm hiểu thêm</a></span></div>
